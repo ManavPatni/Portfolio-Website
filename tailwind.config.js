022d7916769d4c75c2cbc9d7c69cae68f242e530
ManavPatni <developer.mnvpatni@gmail.com>
2024-08-01 11:04:18 +0530

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      "pure-black":"#000000",
+      "light-grey":"#D7D7D7"
+    }
+  },
+  plugins: [],
+}
+
