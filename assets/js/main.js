6c02be5c5e16929c658b847acbbd6638e2e84d4c
ManavPatni <developer.mnvpatni@gmail.com>
2024-08-04 13:08:06 +0530

    Fixed Screen Breakpoints. And added animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,9 +38,51 @@ window.addEventListener('scroll', blurHeader)
 
 
 /* SHOW SCROLL UP */ 
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this,scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true // Animations repeat
+})
+
+//Home, experience, skills
+sr.reveal(`.home__data, .experience, .skills, .contact__container`)
+sr.reveal(`.home__img`, {delay: 600})
+sr.reveal(`.home__scroll`, {delay: 800})
+
+//Info
+sr.reveal(`.about__content`, {origin: 'right'})
+sr.reveal(`.about__img`, {origin: 'left'})
+
+//Project
+sr.reveal(`.project__card, .services`, {interval: 100})
